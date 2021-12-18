90a4a9dcae608bb8532f2ea4001cacc72c4311aa
Kun <ah09program@gmail.com>
2021-12-18 13:12:02 +0700

    Chỉnh sửa lặt vặt

--- Final_project/BarcodeDetection_Camera.py
+++ Final_project/BarcodeDetection_Camera.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 def count_children(hierarchy, parent, inner=False):
-    # condition break
     if parent == -1:
         return 0
     elif not inner:
@@ -24,28 +23,27 @@ def AreaDetection(image):
     temp_c = sorted(cnts, key=cv2.contourArea, reverse=True)
 
     areaDetect = np.zeros_like(dilate)
+
     if len(temp_c) != 0:
         temp_c_filter = [temp for temp in temp_c if temp.shape[0] > 10]
         for rect in temp_c_filter:
             rect = cv2.minAreaRect(rect)
             box = np.int0(cv2.boxPoints(rect))
             cv2.drawContours(areaDetect,[box],-1,(255,255,255),-1)
-
     areaDetect = cv2.dilate(areaDetect, None, iterations=15)
-    cv2.imshow("a",areaDetect)
     checkareaDetect = cv2.bitwise_and(areaDetect,np.bitwise_not(dilate))
-    cv2.imshow("checka", checkareaDetect)
     cnts,_ = cv2.findContours(areaDetect.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
     _, hierarchy = cv2.findContours(checkareaDetect.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
     detect_c = sorted(cnts, key=cv2.contourArea, reverse=True)
     if len(detect_c) != 0:
         i=0
         max = 0
-        maxRect = np.max([detect.shape[0] for detect in detect_c])
         for detect in detect_c:
+            print(detect)
+            print("EndDetect")
             if (hierarchy is not None) and (count_children(hierarchy[0],i)!=0):
                 if count_children(hierarchy[0],i) > max: max = count_children(hierarchy[0],i)
-            if count_children(hierarchy[0],i) >= 40 and detect.shape[0] == maxRect:
+            if count_children(hierarchy[0],i) >= 35 and detect.shape[0] == detect_c[0].shape[0]:
                 x, y, w, h = cv2.boundingRect(detect)
                 rect = cv2.minAreaRect(detect)
                 box = np.int0(cv2.boxPoints(rect))
@@ -54,5 +52,4 @@ def AreaDetection(image):
             i+=1
         print(max)
         print("break")
-
     return image,out
